7aca7bfcc31a6e2c6c6a58442377c350eb2f43f8
skirik2020 <78725374+skirik2020@users.noreply.github.com>
2026-09-03 20:38:10 +0300

    deploy: share button + pass text

---
+++ _next/static/NtsMcpTOJ38dyfo9O6oGE/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/30daypass-web//_next/:path+",
+        "destination": "/30daypass-web/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
